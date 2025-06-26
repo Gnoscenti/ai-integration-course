3660c46df079f5bab0e24fe05adebed4b76449b4
Gnoscenti <bpcca@icloud.com>
2025-06-26 09:22:27 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Layout />}> {/* Use Layout to wrap routes */}
           <Route index element={<HomePage />} />
           {/* Updated route for courses to be more generic, actual course selection can happen in CourseOverviewPage or a new CoursesListPage */}
-          <Route path="courses" element={<CourseOverviewPage />} /> 
+          <Route path="/courses" element={<CourseOverviewPage />} /> 
           {/* Updated route for specific lessons to include course and module context */}
           <Route path="courses/:courseId/modules/:moduleId/lessons/:lessonId" element={<LessonPage />} />
           <Route path="recap" element={<PersonalizedRecapPage />} />
